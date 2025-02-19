5bdd5285d8be9fdee1af8e037ca7b40ed44ea601
Matthieu Fouquet <mfouquet@sqli.com>
2025-02-19 11:54:55 +0100

    add storage account with restricted naming convention

--- main.tf
+++ main.tf
@@ -1,11 +1,28 @@
 # rédiger le code d'infra principal
-
+locals {
+  base_name = "${var.project}-${var.environment}"
+  restricted_base_name = lower(replace(local.base_name, "-", ""))
+}
 
 resource "azurerm_log_analytics_workspace" "main" {
-  name                = "log-${var.project}-${var.environment}"
+  name                = "log-${local.base_name}"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
+resource "random_string" "storage_account_suffix" {
+  length = 6
+  special = false
+  upper = false
+  numeric = true
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "st${local.restricted_base_name}${random_string.storage_account_suffix.result}"
+  location                 = data.azurerm_resource_group.main.location
+  resource_group_name      = data.azurerm_resource_group.main.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
