a6fa7dd00a9c56084f07582cef45a5e7ad8ea442
Matthieu Fouquet <mfouquet@sqli.com>
2025-02-19 15:52:37 +0100

    add outputs for mssql and rabbitmq fqdn

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,15 @@
 # définir les valeurs de sortie de Terraform
+output "azurerm_mssql_server_fqdn" {
+  description = "Fully qualified domain name of the SQL Server"
+  value       = azurerm_mssql_server.main.fully_qualified_domain_name
+}
+
+output "azurerm_ci_rabbitmq_fqdn" {
+  description = "Fully qualified domain name of the RabbitMQ server"
+  value       = azurerm_container_group.rabbitmq.fqdn
+}
+
+output "rabbitmq_webui_url" {
+  description = "URL of the RabbitMQ Management Web UI"
+  value       = "http://${azurerm_container_group.rabbitmq.fqdn}:15672/"
+}
