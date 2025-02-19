c9e04d22180601eb2e94077e73b208731d2ebd30
Matthieu Fouquet <mfouquet@sqli.com>
2025-02-19 10:47:18 +0100

    add providers file

---
+++ providers.tf
@@ -0,0 +1,15 @@
+# définir les providers utilisés
+# doc azurerm provider : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.19.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
